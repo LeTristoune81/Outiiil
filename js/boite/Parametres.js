139b67316933714b84d69ac9682a0a6bca5fde7e
LeTristoune81 <pierre.pitou81@gmail.com>
2025-05-05 15:11:25 +0200

    Parametres.js

--- js/boite/Parametres.js
+++ js/boite/Parametres.js
@@ -133,18 +133,5 @@ class BoiteParametre extends Boite
         </form>`);
         return this;
     }
-	     // après les autres paramètres...
-this.parametres["uniteAntisondeTerrain"] = new Parametre({
-cle: "uniteAntisondeTerrain",
-libelle: "Antisonde – terrain",
- type: "number",
-valeurDefaut: 1
-});
- this.parametres["uniteAntisondeDome"]    = new Parametre({
- cle: "uniteAntisondeDome",
-libelle: "Antisonde – dôme",
- type: "number",
-valeurDefaut: 1
-});
 
 }
