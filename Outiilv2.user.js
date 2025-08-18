8f89384e55a2bce19122a1c4bb37d037b7e19609
LeTristoune81 <pierre.pitou81@gmail.com>
2025-08-18 12:08:03 +0200

    Update Outiilv2.user.js

--- Outiilv2.user.js
+++ Outiilv2.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Outiil v2
-// @version     2.2.3
+// @version     2.2.4
 // @author      Hraesvelg, modifié par White
 // @downloadURL  https://raw.githubusercontent.com/LeTristoune81/Outiiil/main/Outiilv2.user.js
 // @updateURL    https://raw.githubusercontent.com/LeTristoune81/Outiiil/main/Outiilv2.user.js
