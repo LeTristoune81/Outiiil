a4f0a5a8eb202425fa04e8c92401f391a9fe8e60
LeTristoune81 <pierre.pitou81@gmail.com>
2025-08-20 16:36:09 +0200

    Update Outiilv2.user.js

--- Outiilv2.user.js
+++ Outiilv2.user.js
@@ -2,8 +2,8 @@
 // @name        Outiil v2
 // @version     2.2.4
 // @author      Hraesvelg, modifié par White
-// @downloadURL https://raw.githubusercontent.com/LeTristoune81/Outiiil/main/Outiilv2.use.user.js
-// @updateURL   https://raw.githubusercontent.com/LeTristoune81/Outiiil/main/Outiilv2.use.user.js
+// @downloadURL https://raw.githubusercontent.com/LeTristoune81/Outiiil/main/Outiilv2.user.js
+// @updateURL   https://raw.githubusercontent.com/LeTristoune81/Outiiil/main/Outiilv2.user.js
 // @description Outiil de Hraesvelg herbergé par Manitas voire ici pour les sources originales : https://github.com/Hraesvelg/Outiiil, existe aussi en extension Chrome
 // @match       http://*.fourmizzz.fr/*
 
