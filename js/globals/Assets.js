fc160f97724a822a647277020162f9ae5582ed5c
LeTristoune81 <pierre.pitou81@gmail.com>
2025-08-22 13:08:00 +0200

    Add files via upload

---
+++ js/globals/Assets.js
@@ -0,0 +1,19 @@
+// js/globals/Assets.js
+// Déclaré en tout premier pour éviter la TDZ (pas de const/let ici)
+var OUTIIIL_IMG_CDN = "https://cdn.jsdelivr.net/gh/LeTristoune81/Outiiil@main/images/";
+
+var IMG_CHANGE      = OUTIIIL_IMG_CDN + "change.png";
+var IMG_ACTUALISER  = OUTIIIL_IMG_CDN + "actualize_on_01.png";
+var IMG_UPDATE      = IMG_ACTUALISER; // alias si certains modules utilisent ce nom
+
+var IMG_CRAYON      = OUTIIIL_IMG_CDN + "crayon.gif";
+var IMG_CROIX       = OUTIIIL_IMG_CDN + "croix.png";
+var IMG_COPIER      = OUTIIIL_IMG_CDN + "copy.png";
+var IMG_HISTORIQUE  = OUTIIIL_IMG_CDN + "historique.png";
+var IMG_LIVRAISON   = OUTIIIL_IMG_CDN + "livraison.png";
+var IMG_RADAR       = OUTIIIL_IMG_CDN + "radar.png";
+var IMG_SPRITE_MENU = OUTIIIL_IMG_CDN + "sprite_menu.png";
+var IMG_UTILITY     = OUTIIIL_IMG_CDN + "utility.png";
+var IMG_DOWN        = OUTIIIL_IMG_CDN + "down.png";
+var IMG_UP          = OUTIIIL_IMG_CDN + "up.png";
+var IMG_OUTIIIL     = OUTIIIL_IMG_CDN + "outiiil.png";
