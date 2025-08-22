7e66a92545ef78a00cce820e0cdf6d4a1c6bbbe8
LeTristoune81 <pierre.pitou81@gmail.com>
2025-08-22 14:02:29 +0200

    Update Assets.js

--- js/globals/Assets.js
+++ js/globals/Assets.js
@@ -1,19 +1,19 @@
 // js/globals/Assets.js
-// Déclaré en tout premier pour éviter la TDZ (pas de const/let ici)
-var OUTIIIL_IMG_CDN = "https://cdn.jsdelivr.net/gh/LeTristoune81/Outiiil@main/images/";
-
-var IMG_CHANGE      = OUTIIIL_IMG_CDN + "change.png";
-var IMG_ACTUALISER  = OUTIIIL_IMG_CDN + "actualize_on_01.png";
-var IMG_UPDATE      = IMG_ACTUALISER; // alias si certains modules utilisent ce nom
-
-var IMG_CRAYON      = OUTIIIL_IMG_CDN + "crayon.gif";
-var IMG_CROIX       = OUTIIIL_IMG_CDN + "croix.png";
-var IMG_COPIER      = OUTIIIL_IMG_CDN + "copy.png";
-var IMG_HISTORIQUE  = OUTIIIL_IMG_CDN + "historique.png";
-var IMG_LIVRAISON   = OUTIIIL_IMG_CDN + "livraison.png";
-var IMG_RADAR       = OUTIIIL_IMG_CDN + "radar.png";
-var IMG_SPRITE_MENU = OUTIIIL_IMG_CDN + "sprite_menu.png";
-var IMG_UTILITY     = OUTIIIL_IMG_CDN + "utility.png";
-var IMG_DOWN        = OUTIIIL_IMG_CDN + "down.png";
-var IMG_UP          = OUTIIIL_IMG_CDN + "up.png";
-var IMG_OUTIIIL     = OUTIIIL_IMG_CDN + "outiiil.png";
+(function (g) {
+  var CDN = "https://cdn.jsdelivr.net/gh/LeTristoune81/Outiiil@main/images/";
+  g.IMG_CHANGE      = g.IMG_CHANGE      || CDN + "change.png";
+  g.IMG_ACTUALISER  = g.IMG_ACTUALISER  || CDN + "actualize_on_01.png";
+  g.IMG_CRAYON      = g.IMG_CRAYON      || CDN + "crayon.gif";
+  g.IMG_CROIX       = g.IMG_CROIX       || CDN + "croix.png";
+  g.IMG_COPIER      = g.IMG_COPIER      || CDN + "copy.png";
+  g.IMG_HISTORIQUE  = g.IMG_HISTORIQUE  || CDN + "historique.png";
+  g.IMG_LIVRAISON   = g.IMG_LIVRAISON   || CDN + "livraison.png";
+  g.IMG_RADAR       = g.IMG_RADAR       || CDN + "radar.png";
+  g.IMG_SPRITE_MENU = g.IMG_SPRITE_MENU || CDN + "sprite_menu.png";
+  g.IMG_UTILITY     = g.IMG_UTILITY     || CDN + "utility.png";
+  g.IMG_DOWN        = g.IMG_DOWN        || CDN + "down.png";
+  g.IMG_UP          = g.IMG_UP          || CDN + "up.png";
+  g.IMG_OUTIIIL     = g.IMG_OUTIIIL     || CDN + "outiiil.png";
+  // Compat “anciens modules” :
+  g.IMG_UPDATE      = g.IMG_UPDATE || g.IMG_ACTUALISER;
+})(window);
